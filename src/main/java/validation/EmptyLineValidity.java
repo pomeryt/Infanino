d5849f261dabc673f100089163e71770aabfd072
revomin <hil00137@gmail.com>
2018-11-25 13:06:47 +0900

    Create EmptyLineValidity.java refs #7

---
+++ src/main/java/validation/EmptyLineValidity.java
@@ -0,0 +1,59 @@
+package validation;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Check if the Line is valid or not.
+ */
+public final class EmptyLineValidity implements Validation {
+	
+	public EmptyLineValidity(final String code) {
+		this.code = code;
+	}
+	
+	@Override
+	public boolean valid() {
+		final String[] lines = this.code.split("\n");
+		final String blankPattern = "^\\s*$";
+		final Pattern pattern = Pattern.compile(blankPattern);
+		
+		for(int index=0;index<lines.length-1;index++) {
+			final Matcher currentLine = pattern.matcher(lines[index]);
+			final Matcher nextLine = pattern.matcher(lines[index+1]);
+			
+			if(currentLine.find() && nextLine.find()) {
+				this.errorLine = index+1;
+				this.validity = false;
+				this.errorMsg = "There are empty lines more than two.";
+				return false;
+			}
+		}
+		
+		this.validity = true;
+		return true;
+	}
+
+	@Override
+	public int line() {
+		if(this.validity) {
+			throw new IllegalStateException("This method can be used when the Empty Line is invalid.");
+		}
+		
+		return this.errorLine;
+	}
+
+	@Override
+	public String reason() {
+		if(this.validity) {
+			throw new IllegalStateException("This method can be used when the Empty Line is invalid.");
+		}
+		
+		return this.errorMsg;
+	}
+	
+	private String errorMsg;
+	private boolean validity;
+	private int errorLine;
+	final private String code;
+}
